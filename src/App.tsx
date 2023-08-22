4324e55fdac85c896f18c8a443d54c5f50df7c5d
Henrique Goxtoso <henrique.marqueti@estudante.ifms.edu.br>
2023-08-22 10:17:23 -0400

    commit da atividade 22/08

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './componentes/header/Header'
 import './App.css'
+import Filme from './componentes/filme/Filme'
 import Footer from './componentes/footer/Footer'
 
 function App() {
   return (
     <div>
    <Header/>
+   <Filme/>
    <Footer/>
    </div>
   )
