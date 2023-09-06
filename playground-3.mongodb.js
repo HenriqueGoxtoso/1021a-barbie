ba2490e080f3f3c042ce015e87bae95684aa61fe
Henrique Goxtoso <henrique.marqueti@estudante.ifms.edu.br>
2023-09-06 08:45:38 -0400

    commit

--- playground-3.mongodb.js
+++ playground-3.mongodb.js
@@ -1,47 +1,126 @@
+use("bdacademia");
+db.dropDatabase();
+use("bdacademia");
+db.createCollection("personal");
+db.personal.insertMany([
+    {_id:1,
+        nome:"Júlio",
+        salario:3000, 
+        alunos:[
+            {
+                nome:"Tere",
+                idade:28
+            },
+            {
+                nome:"João",
+                idade:34
+            },
+            {
+                nome:"Pedro",
+                idade:60
+            }
+        ]
+    },
+    {_id:2,nome:"Adriano",salario:3000,alunos:[]},
+    {_id:3,nome:"Thaina",salario:4000,alunos:[]}
+])
 
+//Como apagar algum dado de uma collection
+// db.personal.drop() //Apaga uma collection
+// db.personal.find()
 
-//Crie um banco de dados chamado mypet no MONGODB :D
-use("mypet")
-//Crie uma collection chamada dono 
-db.createCollection("dono")
-// 3. Crie as colunas na `collection dono`: `_id` `nome` `idade` `animal` 
-// 1. Na coluna `animal`, adicione as informações do: `_id` `nome` `valor` `raca`
-// 2. Insira 4 itens na `collection` `dono` 
+db.personal.deleteMany({})
+
+db.createCollection("treino");
+db.treino.insertOne(
+  {
     
-//     *(OBS: os ids devem ser em sequencia iniciando pelo 1)*
-    
-// 3. Para cada item adicionado na `collection dono`, adicione 3 itens em `animal`
-db.dono.insertMany(
-  {_id:1, nome:"Maria Eduarda", idade: 18, 
-  animal:[{_id: 1, nome: "Maggy", valor:"1000", raca:"Pincher"},
-  {_id: 2, nome: "Mago", valor:"1000", raca:"Bacê"},
-  {_id: 3, nome: "Mongo", valor:"1000", raca:"Bulldog"}]
-},
-{_id:2, nome:"Henrique", idade: 18, 
-animal:[{_id: 4, nome: "Lutor", valor:"1000", raca:"Vira Lata"},
-{_id: 5, nome: "Lex", valor:"1000", raca:"Vira Lata"},
-{_id: 6, nome: "Capado", valor:"1000", raca:"Vira Lata"}]
-},
-{_id:3, nome:"Maria Eduarda", idade: 18, 
-animal:[{_id: 7, nome: "Laranja", valor:"1000", raca:"Papagaio"},
-{_id: 8, nome: "Polaco", valor:"1000", raca:"Siamês"},
-{_id: 9, nome: "Lotus", valor:"1000", raca:"Periquito"}]
-},
-{_id:4, nome:"Maria Eduarda", idade: 18, 
-animal:[{_id: 10, nome: "Molho", valor:"1000", raca:"Pincher"},
-{_id: 11, nome: "Lucifer", valor:"1000", raca:"Persa"},
-{_id: 12, nome: "Alexander", valor:"1000", raca:"Kakatua"}]
-}
+    dia:"Segunda",
+    nome:"Quadriceps",
+    exercicios:[
+      {
+        nome:"Agachamento Livre",
+        serie:5,
+        repeticoes:12,
+        peso:100
+      },
+      {
+        nome:"Leg 45",
+        serie:5,
+        repeticoes:12,
+        peso:200
+      },
+      {
+        nome:"Agachamento Hack",
+        serie:5,
+        repeticoes:12,
+        peso:200
+      },
+      {
+        nome:"Extensora",
+        serie:5,
+        repeticoes:12,
+        peso:200
+      }
+    ]
+  }
 )
-//Faça uma consulta que liste o nome e animal do dono com _id 2
-db.dono.find({_id:2},{nome:true, animal:true})
-//Faça uma consulta que apague o dono com id 2
-db.dono.deleteMany({_id:2})
-//Faça uma consulta que liste todos os dono
-db.dono.find({})
-//Faça uma consulta que apague todos os dados da collection dono 
-db.dono.deleteMany({})
-//Faça uma consulta que exclua a collection dono
-db.dono.drop()
-//Faça uma consulta que exclua o banco de dados mypet  
-db.dropDatabase()
+db.treino.insertOne(
+  {
+    
+    dia:"Segunda",
+    nome:"Costas",
+    exercicios:[
+      {
+        nome:"Barra Fixa",
+        serie:5,
+        repeticoes:6,
+        peso:20
+      },
+      {
+        nome:"Remada Baixa",
+        serie:4,
+        repeticoes:15,
+        peso:60
+      },
+      {
+        nome:"Remada Cavalo",
+        serie:5,
+        repeticoes:12,
+        peso:80
+      },
+      {
+        nome:"Puxada Alta",
+        serie:5,
+        repeticoes:12,
+        peso:70
+      }
+    ]
+  }
+)
+
+use("bdacademia")
+//db.treino.find({})
+//db.treino.find({nome:"Costas"})
+db.treino.find({_id:ObjectId ("64f866dd30773953278c3fff")})
+
+use("bdacademia")
+db.treino.updateOne(
+  {_id:ObjectId ("64f866dd30773953278c3fff")},
+  {
+    $set:{dia:"Sexta"}
+  }
+)
+
+use("bdacademia")
+db.treino.updateOne(
+  {_id:ObjectId("64f866dd30773953278c3fff"),"exercícios.nome":"Puxada Alta"},
+  {
+    $set:{
+      dia:"Sexta",
+      "exercicios.$.repeticoes":15,
+      "exercicios.$.serie":4,
+      "exercicios.$.peso":80
+    }
+  }
+)
