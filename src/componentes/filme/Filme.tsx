da2b4d3911a7f279965d48590dc4570d3863c038
Henrique Goxtoso <henrique.marqueti@estudante.ifms.edu.br>
2023-09-12 10:13:53 -0400

    commit 12/09

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -19,7 +19,7 @@ function Filme(props:FilmeProps){
             {/* Se não apareceu a imagem é provavel que você não tenha a imagem na pasta assets */}
             {/* Pode ser no componente, mas o caminho deve estar correto. */}
             {/* Imagem está no meu git (Não) */}
-            <img className="img-filme" src={props.imagem} alt="" />
+            
             <div className="text-barbie">
                 <h1>{props.titulo}</h1>
                 <p className="sinopse">{props.sinopse}</p>
--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,25 +1,111 @@
+import { useState } from 'react'
 import Filme from './../filme/Filme'
 import './Main.css'
-export default function Main(){
-    return(
-        <main className="content-main">
-            <input className='pesquisa' type="text" placeholder='Pesquise um filme' />
-            <p>Digitado:</p>
-            <Filme titulo='Barbie'
-            sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-            imagem='/barbie.png'/>
-            <Filme titulo='Barbie'
-            sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-            imagem='/bumb.jpg'/>
-            <Filme titulo='Barbie'
-            sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-            imagem='/barbie.png'/>
-            <Filme titulo='Barbie'
-            sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-            imagem='/barbie.png'/>
-            <Filme titulo='Barbie'
-            sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-            imagem='/barbie.png'/>
-        </main>
+type FilmeType{
+    id:number,
+    titulo:string,
+    sinopse:string,
+    imagem:string
+
+}
+export default function Main() {
+    //let textodigitado = 'Barbie'
+    const [texto,setTexto]=useState("Barbie")
+    const filmes =[
+        
+        /*{
+            id:1,
+            titulo:'Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        },
+        {
+            id:2,
+            titulo:'Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        },
+        {
+            id:3,
+            titulo:'Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        }*/
+    ]
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+        
+        console.log(e.target.value)
+        setTexto(e.target.value)
+    }
+    return (
+        <>
+            <div className="campo_pesquisa">
+                <p>Busque um filme</p>
+                <input type="text" className='botao_pesquisa' placeholder='Pesquise um Filme' onChange={TrataTexto}/>
+                {
+                    (texto)?<p>Resultados para:{texto}</p>:""
+                }
+                
+            </div>
+            <main className="content-main">
+                <Filme titulo='Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/barbie.png'
+                />
+                <Filme titulo='Filme Barbie'
+                    sinopse='Depois de ser expulsa da 
+                    Barbieland por ser uma boneca de aparência 
+                    menos do que perfeita, Barbie parte para o 
+                    mundo humano em busca da verdadeira felicidade.'
+                    imagem='/KEN.png'
+                />
+                <Filme titulo='Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/barbie.png'
+                />
+                <Filme titulo='Filme Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/boneca.jpg'
+                />
+                <Filme titulo='Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/barbie.png'
+                />
+                <Filme titulo='Filme Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/boneca.jpg'
+                />
+                <Filme titulo='Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/barbie.png'
+                />
+                <Filme titulo='Filme Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/boneca.jpg'
+                />
+
+            </main>
+        </>
     )
 }
