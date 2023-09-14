6722cc4ea9a2208edcafcc1609f0d3e43fc219d1
Henrique Goxtoso <henrique.marqueti@estudante.ifms.edu.br>
2023-09-14 08:26:20 -0400

    commit 14/09

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -6,30 +6,15 @@ type FilmeType{
     titulo:string,
     sinopse:string,
     imagem:string
-
 }
 export default function Main() {
     //let textodigitado = 'Barbie'
-    const [texto,setTexto]=useState("Barbie")
-    const filmes =[
-        
-    <main className="content-main">
-        {filmes
-                     .filter((filme) => {
-                     const textoSemEspacos = texto.replace(/\s/g, ''); // Remove espaços em branco do texto
-                     const tituloSemEspacos = filme.titulo.replace(/\s/g, ''); // Remove espaços em branco do título do filme
-                     const sinopseSemEspacos = filme.sinopse.replace(/\s/g, ''); // Remove espaços em branco da sinopse do filme
-                     const tudo = tituloSemEspacos.toLowerCase().includes(textoSemEspacos.toLowerCase());
-                     const tudo2 = sinopseSemEspacos.toLowerCase().includes(textoSemEspacos.toLowerCase());
-                     const tudo3 = tudo || tudo2; // Use '||' (ou) para permitir que qualquer uma das condições seja verdadeira
-                     return tudo3;
-                     })
-                     .map((filme) => (
-                     <Filme key={filme.id} titulo={filme.titulo} sinopse={filme.sinopse} imagem={filme.imagem} />
-                     ))}
-</main>
-        
-        /*{
+    //Hooks são funções do React que ajudam a gente a fazer tarefas
+    //específicas
+    const [texto,setTexto]=useState("")
+
+    const filmes:FilmeType[] = [
+        {
             id:1,
             titulo:'Barbie',
             sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
@@ -37,34 +22,58 @@ export default function Main() {
         },
         {
             id:2,
+            titulo:'Filme Barbie',
+            sinopse:'Depois de ser expulsa da Barbieland por ser.',
+            imagem:'/KEN.png'
+        },
+        {
+            id:3,
+            titulo:'Filme Barbie',
+            sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+            imagem:'/boneca.jpg'
+        },
+        {
+            id:5,
             titulo:'Barbie',
             sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
             imagem:'/barbie.png'
         },
-        {
-            id:3,
-            titulo:'Barbie',
-            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
-            imagem:'/barbie.png'
-        }*/
     ]
+
+    //O parâmetro "e" da minha função será o meu evento que ocorreu
     function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
-        
-        console.log(e.target.value)
+        //console.log(e.target.value)
+        //Como eu faço para mudar o texto para "TERE"
         setTexto(e.target.value)
     }
     return (
         <>
             <div className="campo_pesquisa">
                 <p>Busque um filme</p>
-                <input type="text" className='botao_pesquisa' placeholder='Pesquise um Filme' onChange={TrataTexto}/>
-                {
-                    (texto)?<p>Resultados para:{texto}</p>:""
-                }
-                
+                <input type="text" 
+                       className='botao_pesquisa'
+                       placeholder='Pesquise um Filme'
+                       onChange={TrataTexto} />
+                {texto && <p>Resultados Para: {texto} </p>}
             </div>
             <main className="content-main">
-                <Filme titulo='Barbie'
+                {/* 
+                    Use algo do vetor para tentar criar os filmes 
+                */}
+                {
+                    filmes.filter((filme)=>filme.titulo.toLowerCase().includes(texto)).map(
+                        (filme)=>
+                            <Filme 
+                                key={filme.id}
+                                sinopse={filme.sinopse}
+                                titulo={filme.titulo}
+                                imagem={filme.imagem}
+                            />
+                    )
+                }
+
+                
+                {/* <Filme titulo='Barbie'
                     sinopse='Depois de ser expulsa da 
                    Barbieland por ser uma boneca de aparência 
                    menos do que perfeita, Barbie parte para o 
@@ -73,9 +82,7 @@ export default function Main() {
                 />
                 <Filme titulo='Filme Barbie'
                     sinopse='Depois de ser expulsa da 
-                    Barbieland por ser uma boneca de aparência 
-                    menos do que perfeita, Barbie parte para o 
-                    mundo humano em busca da verdadeira felicidade.'
+                   Barbieland por ser.'
                     imagem='/KEN.png'
                 />
                 <Filme titulo='Barbie'
@@ -119,7 +126,7 @@ export default function Main() {
                    menos do que perfeita, Barbie parte para o 
                    mundo humano em busca da verdadeira felicidade.'
                     imagem='/boneca.jpg'
-                />
+                /> */}
 
             </main>
         </>
